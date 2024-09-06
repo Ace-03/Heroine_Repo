e7638ce794b6c1aeccf9ba2c40f5397bc5daf335
Ace-03 <66760646+Ace-03@users.noreply.github.com>
2024-09-05 20:05:26 -0700

    created build

---
+++ Heroine/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    enum State
+    {
+        STATE_STANDING,
+        STATE_JUMPING,
+        STATE_DUCKING,
+        STATE_DIVING
+    };
+}
---
+++ Heroine/Assets/Scripts/Heroine.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heroine : MonoBehaviour
+{
+    void Heroine::handleInput(Input input)
+    {
+
+    }  
+}
